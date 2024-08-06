79c825e66ee8f27c9b97b771ffed7a93008a94c1
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-08-06 13:41:34 +0300

    Add files via upload

---
+++ 03. Encapsulation/01.SortPeople/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string firstName, string lastName, int age) 
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+
+    }
+}
