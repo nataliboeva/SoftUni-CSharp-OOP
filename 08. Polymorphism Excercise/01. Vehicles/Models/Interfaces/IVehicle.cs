941710a7d5a07203c98daf1cc17a338336eace1d
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-08-08 15:05:07 +0300

    Add files via upload

---
+++ 08. Polymorphism Excercise/01. Vehicles/Models/Interfaces/IVehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles.Models.Interfaces
+{
+    public interface IVehicle
+    {
+        double Fuel { get; }
+        double Consumption { get; }
+
+        string Travel(double distance);
+        void Refuel(double liters);
+    }
+}
