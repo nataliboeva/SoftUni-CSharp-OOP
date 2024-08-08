d3fe92edbbfe9a3a45671ef3da230606a2971bb3
Natali Boeva <121763213+nataliboeva@users.noreply.github.com>
2024-08-08 15:04:47 +0300

    Add files via upload

---
+++ 08. Polymorphism Excercise/01. Vehicles/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles.Models
+{
+    public class Car : BaseVehicle
+    {
+        public Car(double fuel, double consumption) : base(fuel, consumption)
+        {
+        }
+
+        public override double Consumption => base.Consumption + 0.9;
+    }
+}
---
+++ 08. Polymorphism Excercise/01. Vehicles/Models/Truck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicles.Models
+{
+    public class Truck : BaseVehicle
+    {
+        public Truck(double fuel, double consumption) : base(fuel, consumption)
+        {
+        }
+
+        public override double Consumption => base.Consumption + 1.6;
+
+        public override void Refuel(double liters) => base.Refuel(liters * 0.95);
+    }
+}
